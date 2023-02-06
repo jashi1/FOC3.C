e996c6f5c5c73628c9c1635edb180d787416c0c9
jashi1 <124244883+jashi1@users.noreply.github.com>
2023-02-06 14:45:28 +0530

    Add files via upload

---
+++ 451.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int year;
+  printf("Enter a year.");
+  scanf("%d",&year);
+  {
+  printf("%dis aleap year",&year);
+  }
+  printf("%d is not a leap year", &year);
+   }
+getch();
+}
